4564c09dc640e2196d684eff372d6deed215740d
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2023-06-17 21:49:34 +0800

    config:项目中配置less

--- craco.config.ts
+++ craco.config.ts
@@ -1,9 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
+
+const resolve = (configUrl : string) => path.resolve(__dirname , configUrl) 
+
 module.exports = {
+  plugins : [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            // modifyVars: { '@primary-color': '#1DA57A' }, // 配置全局 less变量
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
   webpack : {
     alias : {
-      "@" : path.resolve(__dirname , 'src')
+      "@" : resolve('src')
     }
   }
 }
