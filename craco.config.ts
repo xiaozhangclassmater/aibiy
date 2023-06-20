31d250b1428aee3515fed86d64ea210f290c286a
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2023-06-20 16:16:58 +0800

    config:feat:新增svg转ReactComponent loader

--- craco.config.ts
+++ craco.config.ts
@@ -1,11 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 const CracoLessPlugin = require('craco-less')
 
-const resolve = (configUrl : string) => path.resolve(__dirname , configUrl) 
+const resolve = (configUrl: string) => path.resolve(__dirname, configUrl)
 
 module.exports = {
-  plugins : [
+  plugins: [
     {
       plugin: CracoLessPlugin,
       options: {
@@ -18,19 +17,31 @@ module.exports = {
       },
     },
   ],
-  devServer : {
-    host : '0.0.0.0',
-    port : 5147,
-    proxy : {
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 5147,
+    proxy: {
       '/api': {
         target: "http://codercba.com:1888/airbnb/api",
         pathRewrite: { '^/api': '' },
       },
     }
   },
-  webpack : {
-    alias : {
-      "@" : resolve('src')
+  webpack: {
+    alias: {
+      "@": resolve('src')
+    },
+    configure: {
+      module: {
+        rules: [
+          {
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+          },
+        ]
+      }
     }
   }
 }
