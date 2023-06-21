3e37fc6e665af72e86b5deaa94ca2c8fdbfbf5b8
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2023-06-21 10:37:31 +0800

    style:样式文件迁移至assets

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,25 @@
 
-import '@/styles/index.less';
+import '@/assets/styles/index.less';
 import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import { ThemeProvider, } from 'styled-components';
 import App from './App';
+import themeConfig from './assets/theme';
 import store from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
     <Suspense fallback='loading'>
       <HashRouter>
         <Provider store={store}>
-          <App />
+         <ThemeProvider theme={themeConfig}>
+         <App />
+         </ThemeProvider>
         </Provider>
       </HashRouter>
     </Suspense>
-  // </React.StrictMode>
 );
