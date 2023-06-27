a5236e5cb66e2fe2b22478914dacafdd66cce856
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2023-06-27 11:23:21 +0800

    update:eslint规则配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,10 @@ module.exports = {
   plugins: [],
   rules: {
     "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/no-unused-vars" : 'off'
+    "@typescript-eslint/no-unused-vars" : 'off',
+    "@typescript-eslint/no-explicit-any" : 'off',
+    "@typescript-eslint/no-empty-function" : 'off',
+    "react-hooks/exhaustive-deps" : 'off',
+    "no-empty" : 'off'
   },
 }
