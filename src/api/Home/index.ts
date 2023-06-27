caef3612e19b17988b141c2099963bf1c2fe4765
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2023-06-27 19:39:49 +0800

    feat:编写高评分接口

--- src/api/Home/index.ts
+++ src/api/Home/index.ts
@@ -1,7 +1,14 @@
 import request from "@/services";
 
 export function getGoodPriceInfo(url: string) {
-  return request<goodPriceResponse>({
+  return request<baseRoomInfo>({
+    url,
+    method: 'get'
+  })
+}
+
+export function getHighscoreInfo(url: string) {
+  return request<baseRoomInfo>({
     url,
     method: 'get'
   })
