fcd7b3aa9bdc377075d7eb9fbd4df01fa3bc314f
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2023-06-27 11:28:58 +0800

    feat:services axios 服务 请求函数接入泛型

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,25 +1,25 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 
 const instance = axios.create({
-  baseURL : process.env.React_APP_BASE_API,
-  timeout : 5000
+  baseURL: process.env.React_APP_BASE_API,
+  timeout: 5000
 })
 
-instance.interceptors.request.use((config :InternalAxiosRequestConfig) => {
+instance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   return config
-} , (err : AxiosError) => {
+}, (err: AxiosError) => {
   return Promise.reject(err)
 })
 
 
-instance.interceptors.response.use((response : AxiosResponse) => {
+instance.interceptors.response.use((response: AxiosResponse) => {
   return response
-},(err :AxiosError) => {
+}, (err: AxiosError) => {
   return err
 })
 
 
-const request = (config : AxiosRequestConfig) => {
-  return instance.request(config)
+function request<T>(config: AxiosRequestConfig) {
+  return instance.request<T>(config)
 }
 export default request
