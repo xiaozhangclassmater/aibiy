8c3b56d58563607f088ecf12a0d71ccab320ad20
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2023-07-02 12:51:38 +0800

    eslint规则更新

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
     "@typescript-eslint/no-explicit-any" : 'off',
     "@typescript-eslint/no-empty-function" : 'off',
     "react-hooks/exhaustive-deps" : 'off',
-    "no-empty" : 'off'
+    "no-empty" : 'off',
+    "@typescript-eslint/no-non-null-assertion" : 'off'
   },
 }
