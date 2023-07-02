9d2374cbb68847b6ae434b9d1dcac53e82750317
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2023-07-02 21:35:02 +0800

    feat:首页使用懒加载

--- src/components/TabCardRoomGroup/index.tsx
+++ src/components/TabCardRoomGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useLazyLoad } from '@/hooks/modules/useLazyLoad'
 import { ReactComponent as RightSvg } from '@/icons/svg/arrow-right.svg'
 import { isEmpty } from '@/utils'
 import { memo, useCallback, useMemo, useState } from 'react'
@@ -10,13 +11,15 @@ import { TabCardRoomGroupWapper } from './style'
 
 interface TabCardRoomGroupProps {
   hotCityProductInfo : HotSourceCityType,
-  proportion? : string | number
+  proportion? : string | number,
+  queryInfo : () => void
 }
 
-const TabCardRoomGroup = memo(( { hotCityProductInfo  , proportion = '33.33%'} : TabCardRoomGroupProps) => {
+const TabCardRoomGroup = memo(( { hotCityProductInfo  , proportion = '33.33%' , queryInfo} : TabCardRoomGroupProps) => {
   const [ products  , setProducts ] = useState<(RoomItem & newAddFiled)[]>([])
   const [ maxRenderCount , setMaxRenderCount] = useState(6)
   const [ currentCityName , setCurrentCityName ] = useState('')
+  const { el : TabCardRef  } = useLazyLoad(queryInfo)
   const computedProportion = useMemo(() => (typeof proportion === 'string' ? proportion : proportion + '%') , [proportion])
   const cacheCurrentCityName = useMemo(() => currentCityName , [currentCityName])
   const getCityHousingResourceInfo = (cityName : string) => {
@@ -27,8 +30,39 @@ const TabCardRoomGroup = memo(( { hotCityProductInfo  , proportion = '33.33%'} :
      
   } , [hotCityProductInfo])
 
+  
+
+    console.log(TabCardRef.current);
+  // useEffect( () => {
+  //   const callback = (entries:IntersectionObserverEntry[]) => {
+
+  //     entries.forEach((entry:IntersectionObserverEntry) => {
+  //       if (entry.isIntersecting) {
+  //         // 元素进入可视区域
+  //         console.log('Element is visible' , entry);
+  //       } else {
+  //         // 元素离开可视区域
+  //         console.log('Element is not visible');
+  //       }
+  //     });
+  //   };
+  //   const options = {
+  //     root: null, // 可视区域的根元素。null 表示整个视口
+  //     rootMargin: '0px', // 可视区域的边缘留白
+  //     threshold: 0, // 目标元素与可视区域的交叉比例
+  //   };
+  //   const observer = new IntersectionObserver(callback , options)
+  //   if (TabCardRef.current) {
+  //     observer.observe(TabCardRef.current); // 监听目标元素
+  //   }
+
+  //   return () => {
+  //     observer.unobserve(TabCardRef.current!)
+  //   }
+  // } , [])
+
   return (
-    <TabCardRoomGroupWapper>
+    <TabCardRoomGroupWapper ref={TabCardRef as any}>
       {
         (!isEmpty(hotCityProductInfo)) && <div className='TabCard-RoomGroup-Wapper'>
           <SectionTitle title={hotCityProductInfo.title || '热门城市'} subTitle={hotCityProductInfo.subtitle || "美丽的城市，邻人向往"} />
