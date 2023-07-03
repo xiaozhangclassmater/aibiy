4becf7facc087083d596a140612829f6be4c0d99
= <9189725+zhang-qingyou@user.noreply.gitee.com>
2023-07-03 13:42:00 +0800

    feat:hooks封装优化

--- src/hooks/modules/useIntersectionObserver.ts
+++ src/hooks/modules/useIntersectionObserver.ts
@@ -1,22 +1,23 @@
-import React, { useEffect } from "react";
+import { isEmpty } from "@/utils";
+import React, { useEffect, useMemo } from "react";
+
+const unRef = (target: React.RefObject<Element>) => target.current!
 
 export default function useIntersectionObserver(
-  target: React.RefObject<HTMLElement | undefined>,
+  target: React.RefObject<Element>,
   callback: (entries: IntersectionObserverEntry[]) => void,
   options?: IntersectionObserverInit
 ) {
+  const targets = useMemo(() => (isEmpty(target) ? [] : (Array.isArray(target) ? target : [target])), [target])
   let observer: IntersectionObserver | null = null
 
   useEffect(() => {
     observer = new IntersectionObserver(callback, options)
-    if (target.current) {
-      observer.observe(target.current)
-    }
+    targets.length > 0 && targets.forEach(el => el.current && observer?.observe(el.current))
   }, [])
 
-
-  function unobserve() {
-    observer && observer.unobserve(target.current!)
+  function unobserve(target: React.RefObject<Element>) {
+    observer && observer.unobserve(unRef(target))
   }
 
   return { unobserve }
--- src/hooks/modules/useLazyLoad.ts
+++ src/hooks/modules/useLazyLoad.ts
@@ -1,12 +1,19 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useIntersectionObserver } from "..";
 export function useLazyLoad(callback: () => void) {
-  const el = useRef<HTMLElement | undefined>()
-  const { unobserve } = useIntersectionObserver(el, ([{ isIntersecting }]: IntersectionObserverEntry[]) => {
+  const Element = useRef<Element>(null)
+  const [isActive, setisActive] = useState(false)
+  const { unobserve } = useIntersectionObserver(Element, ([{ isIntersecting }]: IntersectionObserverEntry[]) => {
     if (isIntersecting) {
+      setisActive(isIntersecting)
       callback()
-      unobserve()
+      unobserve(Element)
     }
+    else {
+      setisActive(false)
+    }
+  }, {
+    threshold: 0
   })
-  return { el }
+  return { Element, isActive }
 }
